95efe7b96030b0738bcfc8fdf60f9f9f22cb28a7
AnnaBoglain <annabogvlad21062003@gmail.com>
2022-12-08 22:32:55 +0300

    Добавлены файлы main.cpp и std.h. Написанна программа для ввода целых чисел

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "std.h"
+#include <locale>
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main() {
+	int a, b;
+	cout << "Введите два числа" << endl;
+	cin >> a;
+	cin >> b;
+
+	return 0;
+}
